f8525bb96b183378b860778e4efd5e977b7d1849
DevMindNit <164946230+DevMindNit@users.noreply.github.com>
2024-04-12 16:04:15 -0400

    Implement draw() function

--- main.cpp
+++ main.cpp
@@ -4,13 +4,21 @@
 
 using namespace std;
 
+// Variables
 bool game_over = false;
 int score = 0;
+int tail_number = 0;
 
-// coordinates
+// Coordinates
+
+// Snake
 int snake_x, snake_y;
 int snake_tail_x[100], snake_tail_y[100];
+// Fuit
 int fruit_x, fruit_y;
+// Console
+const int width = 50;
+const int height = 25;
 
 enum DIRECTION
 {
@@ -31,7 +39,52 @@ void initializeGame()
 
 void draw()
 {
+    // Top border
+    for (int i = 0; i < width + 2; i++)
+        cout<<"#";
+    cout<<endl;
 
+    for (int i = 0; i < height; i++)
+    {
+        // Left border
+        cout<<"#";
+
+        for (int j = 0; j < width; j++)
+        {
+            // Drawing snake, fruit and empty spaces
+            if (i == snake_y && j == snake_x)
+                cout<<"O"; // Snake head
+            else if (i == fruit_y && j == fruit_x)
+                cout<<"X"; // Fruit
+            else
+            {
+                bool print_tail = false;
+
+                // Checking for snake tail segments
+                for (int k = 0; k < tail_number; k++)
+                {
+                    if (snake_tail_x[k] ==j && snake_tail_y[k] == i)
+                    {
+                        cout<<"o"; // Snake tail
+                        print_tail = true;
+                        break; // Exit loop once snake taile segment is found
+                    }
+                }
+                if (!print_tail)
+                    cout<<" "; // Empty space
+            }
+            // Right border
+            if (j == width - 1)
+                cout<<"#";
+        }
+        cout<<endl;
+    }
+    // Bottom border
+    for (int i = 0; i< width + 2; i++)
+        cout<<"#";
+    cout<<endl;
+
+    cout<<"Score: "<<score<<endl;
 }
 
 void userInput()
